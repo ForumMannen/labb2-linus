722a96179c503f189eb2c2d57aa013b44179ccec
Linus Lundell <lundell.linus@gmail.com>
2023-04-21 08:48:09 +0200

    had to reinstall and add everything again

---
+++ themes/storefront-child/functions.php
@@ -0,0 +1,32 @@
+<?php
+//Tar bort sidebar genom filter
+remove_action('woocommerce_sidebar', 'remove_sidebar');
+
+function remove_sidebar()
+{
+    return false;
+}
+
+add_filter('is_active_sidebar', 'remove_sidebar', 10, 2);
+
+add_action('after_setup_theme', 'my_remove_product_result_count', 99);
+
+function my_remove_product_result_count()
+{
+    remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_result_count', 20);
+}
+
+add_action('after_setup_theme', 'my_remove_catalog_ordering', 99);
+
+function my_remove_catalog_ordering()
+{
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10);
+}
+
+add_action('storefront_before_content', 'free_shipping');
+
+function free_shipping()
+{
+    echo '<div class="test">' . "Fri frakt vid köp över 299:-" . '</div>';
+}
